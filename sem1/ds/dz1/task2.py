0f305c0ae54f5a686ea3b0706ea32cbfc0a1e5ee
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-07-10 22:55:41 +0300

    add homework 1

---
+++ sem1/ds/dz1/task2.py
@@ -0,0 +1,15 @@
+# Напишите программу для проверки истинности утверждения 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def checkPredicat(a, b, c):
+    return not (x and y and z) == (not x or not y or not y)
+
+rangeBool = [True, False]
+result = 'Утверждение ложно'
+for x in rangeBool:
+    for y in rangeBool:
+        for z in rangeBool:
+            if checkPredicat(x, y, z):
+                result = 'Утверждение истинно'
+print(result)
+                
