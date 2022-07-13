706795e49a500224460d49f0ac5aacd4f31e4b7a
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-07-14 01:24:59 +0300

    add sem1 task1

---
+++ sem2/dz/task1.py
@@ -0,0 +1,25 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+def convertFloatToInt(num):   #перемещение точки пока не исчезнет дробная часть
+    roundCount = 12
+    while (round(num % 1, roundCount)) > 0:
+        roundCount -=1
+        num = int(num) * 10 + round(num % 1, roundCount) * 10
+    return int(num)
+
+def printSumNumbers(num):   #подсчет и печать суммы цифр
+    sum = 0
+    while num  > 0:
+        sum += num % 10
+        num = num // 10
+    print(sum)
+    
+checkInput = False
+while not checkInput:
+    try:
+        n = float(input('Введите вещественное число: '))
+        checkInput = True 
+    except:
+        print('Введите  вещественное число корректно')
+print(convertFloatToInt(n))
+printSumNumbers(convertFloatToInt(n))
