9e4ed1409874ccf567e5b2de63f2347e6ca5b2d3
VinokurovDmitriy <78742044+VinokurovDmitriy@users.noreply.github.com>
2022-07-21 15:28:31 +0300

    Update task3.py
    переделал

--- sem4/dz/task3.py
+++ sem4/dz/task3.py
@@ -1,7 +1,12 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет
 # список неповторяющихся элементов исходной последовательности.
 
-myList = [23, 454, 63, 453, 543, 45, 435, 453, 5435, 45]
-mySet = set(myList)
-print(myList)
-print(mySet)
+myList = [1, 2, 3, 4, 3, 2, 4, 2, 6, 7, 2]
+newList = []
+for index in range(len(myList)):
+    check = True
+    for i in range(index + 1, len(myList)):
+        if myList[index] == myList[i]:
+            check = False
+    if check: newList.append(myList[index])
+print(newList)
